3f7eb570f4d44fce3212a2ee31c4d41d946bba11
Rishab627 <merishabjoshi@gmail.com>
2024-07-06 21:42:37 +0545

    Tech.js

--- src/pages/home/Tech.js
+++ src/pages/home/Tech.js
@@ -5,14 +5,17 @@ const Tech = () => {
   return (
     <div >
 
-      <h1 className='text-5xl font-bold text-center'>Technologies I Use</h1>
-       <div className="dev-icons grid grid-cols-4 justify-items-center space-y-8 gap-10">
-       <DiAndroid size={150} />
+      <h1 className='text-5xl font-bold text-center space-y-10'>Technologies I Use</h1>
+       <div className="dev-icons grid grid-cols-4 justify-items-center space-y-8 gap-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 p-2">
+       
+       
+
        <DiPython size={150} />
+       <DiAndroid size={150}/>
        <DiCss3 size={150} />
        <DiReact size={150} />
        <DiHtml5 size={150} />
-       <DiJavascript size={150} />
+       <DiJavascript size={150} className='hover:text-red-600'  />
        <DiNodejs size={150} className='animate-bounce' />
        <DiAppstore size={150} />
       
