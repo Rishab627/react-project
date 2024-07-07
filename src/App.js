e1a7782b14a8caf1a2d4132dda81b1bca0a0deec
Rishab627 <merishabjoshi@gmail.com>
2024-07-07 13:49:34 +0545

    k

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
-import DisplaySec from './pages/home/DisplaySec'
-import Tech from './pages/home/Tech'
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <DisplaySec />
-      <Tech />
-    
+
 
       
     </div>
